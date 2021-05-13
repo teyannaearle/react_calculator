4997ac04d7c3a3db7ce66d208a1dc6c5adb68e3e
Teyanna Earle <teyonna.earle92@gmail.com>
2021-05-12 21:31:49 -0400

    Removed excess variables and cleaned up code

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -10,15 +10,12 @@ export class Calculator extends Component {
       display: "0",
       operator: "",
       firstNum: 0,
-      secondNum: 0, 
+      secondNum: 0,
       calculated: false,
-      wipeScreen: false,
+      wipeScreen: true,
       equalPressed: false,
-      waitingForInput: false
+      waitingForInput: false,
     };
-    this.answer = "";
-    this.displayCopy = "";
-    this.formatter = new Intl.NumberFormat("en");
   }
 
   handleClick = (e) => {
@@ -26,80 +23,96 @@ export class Calculator extends Component {
   };
 
   displayNumber = (e) => {
-    let input = e.target.value
-         
-    if (this.state.display !=="0" && input === "0"){
-        this.setState({display: "0"})
-        this.displayCopy=""
+    const input = e.target.value;
+    const { display } = this.state;
+
+    if (display !== "0" && display !== 0 && input === "0") {
+      this.setState({ display: "0", firstNum: "0" });
     }
 
-    if (input !== "0"){
-        this.displayCopy = input
-        this.setState({display: this.formatter.format(input)})
-    }     
+    if (input !== "0") {
+      this.setState(
+        {
+          display: input,
+          wipeScreen: false,
+        },
+        function () {
+          this.setNums();
+        }
+      );
+    }
+  };
 
-    if (this.state.waitingForInput ){
-        this.setState({secondNum: this.displayCopy})
-    } else if (!this.state.waitingForInput && !this.state.calculated){
-        this.setState({firstNum:this.displayCopy})
+  setNums = () => {
+    const { display, calculated, equalPressed, waitingForInput } = this.state;
 
-    } else if (!this.state.waitingForInput && this.state.calculated && this.state.equalPressed){
-        this.setState({firstNum:this.displayCopy})
+    if (waitingForInput) {
+      this.setState({ secondNum: display });
+    } else if (!waitingForInput && !calculated) {
+      this.setState({ firstNum: display });
+    } else if (!waitingForInput && calculated && equalPressed) {
+      this.setState({ firstNum: display });
     } else {
-        this.setState({secondNum: this.displayCopy})
+      this.setState({ secondNum: display });
     }
 
-    this.setState({wipeScreen: false})
-
+    this.setState({ wipeScreen: false });
   };
 
   buildNumber = (e) => {
-    this.displayCopy = this.displayCopy + e.target.value;
-    this.setState({ display: this.formatter.format(this.displayCopy) });
-
-    if (!this.state.waitingForInput) {
-      this.setState({ firstNum: this.displayCopy });
-    } else {
-      this.setState({ secondNum: this.displayCopy });
-    }
-
+    const { display } = this.state;
+    this.setState(
+      {
+        display: display + e.target.value,
+      },
+      function () {
+        this.setNums();
+      }
+    );
   };
 
   switcheroo = () => {
-    if (this.state.display !== "0") {
-      this.displayCopy = this.displayCopy * -1;
-      this.setState({ display: this.formatter.format(this.displayCopy) });
+    const { display } = this.state;
 
-        if (!this.state.waitingForInput) {
-            this.setState({ firstNum: this.displayCopy });
-        } else {
-            this.setState({ secondNum: this.displayCopy });
-        }    
+    if (display !== "0" && display !== 0) {
+      this.setState(
+        {
+          display: display * -1,
+        },
+        function () {
+          this.setNums();
+        }
+      );
     }
-
   };
 
   allClear = () => {
-    this.answer = "";
-    this.displayCopy = "";
-    this.setState({ display: "0" });
-    this.setState({ operator: "" });
-    this.setState({ firstNum: 0 });
-    this.setState({ secondNum: 0 });
-    this.setState({ calculated: false });
-    this.setState({ wipeScreen: false });
-    this.setState({ waitingForInput: false });
+    this.setState({
+      answer: "",
+      display: "0",
+      operator: "",
+      firstNum: 0,
+      secondNum: 0,
+      calculated: false,
+      wipeScreen: true,
+      equalPressed: false,
+      waitingForInput: false,
+    });
   };
 
   operator = (e) => {
-    this.setState({ waitingForInput: true });
-    this.setState({ operator: e.target.value });
-    this.setState({ calculated: false });
-    this.setState({ equalPressed: false });
+    const { firstNum, display, secondNum } = this.state;
+    this.setState({
+      waitingForInput: true,
+      operator: e.target.value,
+      calculated: false,
+      equalPressed: false,
+    });
 
-    if (this.state.firstNum === 0) {
-      this.setState({ firstNum: this.displayCopy });
-    } if (this.state.firstNum && this.state.secondNum) {
+    if (firstNum === 0) {
+      this.setState({ firstNum: display });
+    }
+    if (firstNum && secondNum) {
       this.calculate();
       this.setState({ waitingForInput: true });
     }
@@ -111,37 +124,39 @@ export class Calculator extends Component {
     const { firstNum, secondNum } = this.state;
     const firstNumber = Number(firstNum);
     const secondNumber = Number(secondNum);
+    this.answer = "";
 
     if (secondNum) {
       let operator = this.state.operator;
       switch (operator) {
         case "+":
           this.answer = firstNumber + secondNumber;
-          this.setState({ display: this.formatter.format(this.answer) });
+          this.setState({ display: this.answer });
           break;
         case "-":
           this.answer = firstNumber - secondNumber;
-          this.setState({ display: this.formatter.format(this.answer) });
+          this.setState({ display: this.answer });
           break;
         case "/":
           this.answer = Math.floor(firstNumber / secondNumber);
-          this.setState({ display: this.formatter.format(this.answer) });
+          this.setState({ display: this.answer });
           break;
         case "*":
           this.answer = firstNumber * secondNumber;
-          this.setState({ display: this.formatter.format(this.answer) });
+          this.setState({ display: this.answer });
           break;
         default:
           break;
       }
 
-      this.setState({ display: this.formatter.format(this.answer) });
-      this.setState({ firstNum: this.answer });
-      this.setState({ secondNum: 0 });
-      this.setState({ waitingForInput: false });
-      this.setState({ calculated: true });
-      this.setState({ wipeScreen: true });
-      this.displayCopy = this.answer;
+      this.setState({
+        display: this.answer,
+        firstNum: this.answer,
+        secondNum: 0,
+        waitingForInput: false,
+        calculated: true,
+        wipeScreen: true,
+      });
     }
   };
 
